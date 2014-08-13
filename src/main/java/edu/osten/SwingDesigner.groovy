2b0181ea85d466b3dd8c2298817599d0e4fe7818
Mikael Ostberg <mikael.ostberg@smartbear.com>
2014-08-13 23:41:31 +0200

    Resolved issue with Line-numbers crashing the JavaFX-thread

--- src/main/java/edu/osten/SwingDesigner.groovy
+++ src/main/java/edu/osten/SwingDesigner.groovy
@@ -11,6 +11,7 @@ import javafx.scene.layout.HBox
 import javafx.scene.layout.Priority
 import javafx.stage.Stage
 import org.fxmisc.richtext.CodeArea
+import org.fxmisc.richtext.LineNumberFactory
 import org.fxmisc.richtext.StyleSpans
 import org.fxmisc.richtext.StyleSpansBuilder
 
@@ -23,7 +24,12 @@ import static com.google.common.io.Files.write
 import static edu.osten.SwingDesignerApp.*
 import static java.awt.Color.RED
 import static java.io.File.createTempFile
+import static java.util.Collections.*
+import static javafx.application.Platform.*
 import static javafx.scene.layout.AnchorPane.*
+import static javafx.scene.layout.HBox.*
+import static javafx.scene.layout.Priority.*
+import static javax.swing.SwingUtilities.*
 
 class SwingDesignerApp extends Application {
 
@@ -39,28 +45,28 @@ class SwingDesignerApp extends Application {
             "switch", "synchronized", "this", "throw", "throws",
             "transient", "try", "void", "volatile", "while"].toArray()
 
-    static final Pattern KEYWORD_PATTERN = Pattern.compile("\\b(" + String.join("|", KEYWORDS) + ")\\b");
+    static final Pattern KEYWORD_PATTERN = Pattern.compile("\\b(" + String.join("|", KEYWORDS) + ")\\b")
 
     String basicImports =
             '''
-                import groovy.swing.SwingBuilder
-                import net.miginfocom.swing.MigLayout
-                import javax.swing.*
-                import java.awt.*
-                import static java.awt.Color.*
+    import groovy.swing.SwingBuilder
+    import net.miginfocom.swing.MigLayout
+    import javax.swing.*
+    import java.awt.*
+    import static java.awt.Color.*
 
         '''
 
-    String exampleCode = '''
-def JComponent build(){
-    new SwingBuilder().
-    panel( layout: new MigLayout('wrap','','')){
-        JLabel aLabel = label(text:"Header 1")
-        button(text:"clickable", actionPerformed: { aLabel.text = 'Oh no! The button was clicked!'})
-    }
-}'''
+    String exampleCode =
+    '''def JComponent build(){
+        new SwingBuilder().
+        panel( layout: new MigLayout('wrap','','')){
+            JLabel aLabel = label(text:"Header 1")
+            button(text:"clickable", actionPerformed: { aLabel.text = 'Oh no! The button was clicked!'})
+        }
+    }'''
 
-    File scriptFile = createTempFile("script", ".groovy");
+    File scriptFile = createTempFile("script", ".groovy")
 
     @Override
     void start(Stage stage) throws Exception {
@@ -70,57 +76,56 @@ def JComponent build(){
 
         CodeArea codeArea = new CodeArea()
         codeArea.replaceText ''
+        codeArea.setParagraphGraphicFactory LineNumberFactory.get(codeArea)
 
         AnchorPane leftPane = new AnchorPane()
-        setTopAnchor(swingNode, 0.0)
-        setLeftAnchor(swingNode, 0.0)
-        setRightAnchor(swingNode, 0.0)
-        setBottomAnchor(swingNode, 0.0)
+        setTopAnchor swingNode, 0.0
+        setLeftAnchor swingNode, 0.0
+        setRightAnchor swingNode, 0.0
+        setBottomAnchor swingNode, 0.0
+
         AnchorPane rightPane = new AnchorPane()
+        rightPane.minWidth = 500
+        setTopAnchor codeArea, 0.0
+        setRightAnchor codeArea, 0.0
+        setBottomAnchor codeArea, 0.0
+        setLeftAnchor codeArea, 0.0
 
         HBox hbox = new HBox(leftPane, rightPane)
-        HBox.setHgrow(rightPane, Priority.SOMETIMES)
-        HBox.setHgrow(leftPane, Priority.ALWAYS)
+        setHgrow rightPane, SOMETIMES
+        setHgrow leftPane, ALWAYS
         leftPane.children.add swingNode
-
-        setTopAnchor(codeArea, 0.0)
-        setRightAnchor(codeArea, 0.0)
-        setBottomAnchor(codeArea, 0.0)
-        setLeftAnchor(codeArea, 0.0)
-
         rightPane.children.add codeArea
-
-
 
         codeArea.textProperty().addListener(new ChangeListener<String>() {
             @Override
             public void changed(ObservableValue<? extends String> arg0, String arg1, String newScript) {
                 try {
-                    codeArea.setStyleSpans(0, computeHighlighting(newScript));
-                    write(basicImports + newScript, scriptFile, UTF_8);
-                    ClassLoader parent = getClass().getClassLoader();
-                    GroovyClassLoader loader = new GroovyClassLoader(parent);
-                    def buildable;
+                    codeArea.setStyleSpans 0, computeHighlighting(newScript)
+                    write basicImports + newScript, scriptFile, UTF_8
+                    ClassLoader parent = getClass().classLoader
+                    GroovyClassLoader loader = new GroovyClassLoader(parent)
+                    def buildable
                     try {
-                        def groovyClass = loader.parseClass(scriptFile);
-                        buildable = groovyClass.newInstance();
+                        def groovyClass = loader.parseClass(scriptFile)
+                        buildable = groovyClass.newInstance()
                     } catch (Exception e) {
                         JLabel errorPanel = new JLabel(text: 'There\'s a disturbance in your groovy')
-                        errorPanel.setForeground(RED)
+                        errorPanel.foreground = RED
                         swingNode.content = errorPanel
-                        SwingUtilities.invokeLater(new Runnable() {
+                        invokeLater(new Runnable() {
                             @Override
                             public void run() {
                                 errorPanel.revalidate()
                                 errorPanel.repaint()
                             }
                         })
-                        return;
+                        return
                     }
-                    Platform.runLater(new Runnable() {
+                    runLater(new Runnable() {
                         @Override
                         public void run() {
-                            SwingUtilities.invokeLater(new Runnable() {
+                            invokeLater(new Runnable() {
                                 @Override
                                 void run() {
                                     try {
@@ -129,38 +134,37 @@ def JComponent build(){
                                         component.revalidate()
                                         component.repaint()
                                     } catch (Exception e) {
-
                                         //silent
                                     }
                                 }
                             })
                         }
-                    });
+                    })
                 }
                 catch (InstantiationException | IllegalAccessException | IOException e) {
-                    return;
+                    return
                 }
             }
         })
         codeArea.replaceText exampleCode
         Scene scene = new Scene(hbox, 1250, 500)
-        scene.getStylesheets().add(SwingDesignerApp.class.getResource("/style.css").toExternalForm())
-        stage.setScene(scene)
+        scene.stylesheets.add SwingDesignerApp.class.getResource("/style.css").toExternalForm()
+        stage.scene = scene
         stage.show()
     }
 
     StyleSpans<Collection<String>> computeHighlighting(String text) {
-        Matcher matcher = KEYWORD_PATTERN.matcher(text);
-        int lastKwEnd = 0;
-        StyleSpansBuilder<Collection<String>> spansBuilder = new StyleSpansBuilder<>();
-        while(matcher.find()) {
-            spansBuilder.add(Collections.emptyList(), matcher.start() - lastKwEnd);
-            spansBuilder.add(Collections.singleton("keyword"), matcher.end() - matcher.start());
-            lastKwEnd = matcher.end();
+        Matcher matcher = KEYWORD_PATTERN.matcher text
+        int lastKwEnd = 0
+        StyleSpansBuilder<Collection<String>> spansBuilder = new StyleSpansBuilder<>()
+        while (matcher.find()) {
+            spansBuilder.add emptyList(), matcher.start() - lastKwEnd
+            spansBuilder.add singleton("keyword"), matcher.end() - matcher.start()
+            lastKwEnd = matcher.end()
         }
-        spansBuilder.add(Collections.emptyList(), text.length() - lastKwEnd);
-        return spansBuilder.create();
+        spansBuilder.add emptyList(), text.length() - lastKwEnd
+        return spansBuilder.create()
     }
 }
 
-Application.launch(SwingDesignerApp.class, args);
+Application.launch(SwingDesignerApp.class, args)
